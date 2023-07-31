f854f7834b93e262933ead5b7c605960d3d39a4d
Samaksh Gupta <samaksh.gupta@live.com>
2023-07-31 22:41:51 +0100

    Intergrate foursquare API.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('postcode/:postcode')
+  getFourSquareDataFromPostCode(
+    @Param('postcode') postcode: string,
+    @Res() res: Response,
+  ) {
+    return this.appService.getFourSquareDataFromPostCode(postcode, res);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,63 @@
 import { Injectable } from '@nestjs/common';
+import { Response } from 'express';
+import axios from 'axios';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getFourSquareDataFromPostCode(postcode: string, res: Response) {
+    const response = await axios.get(
+      `https://api.postcodes.io/postcodes/${postcode}`,
+    );
+    const { latitude, longitude } = response.data.result;
+    const foursquarePlaces = await axios.get(
+      `https://api.foursquare.com/v3/places/search?ll=${
+        latitude.toString() + ',' + longitude.toString()
+      }`,
+      {
+        headers: {
+          Accept: 'application/json',
+          Authorization: process.env.API_KEY,
+        },
+      },
+    );
+
+    const locationData = foursquarePlaces.data.results.map((el) => {
+      return {
+        name: el.name,
+        fsqId: el.fsq_id,
+        imageUrl:
+          el.categories?.[0]?.icon?.prefix + el.categories?.[0]?.icon?.suffix,
+        latitude: el.geocodes.main.latitude,
+        longitude: el.geocodes.main.longitude,
+        distance: el.distance,
+      };
+    });
+
+    // initializing the CSV string content with the headers
+    let csvData =
+      ['name', 'FSQ ID', 'Image URL', 'Latitude', 'Longitude', 'Distance'].join(
+        ',',
+      ) + '\r\n';
+    locationData.forEach((location) => {
+      // populating the CSV content
+      // and converting the null fields to ""
+      csvData +=
+        [
+          location.name,
+          location.fsqId,
+          location.imageUrl,
+          location.latitude,
+          location.longitude,
+          location.distance,
+        ].join(',') + '\r\n';
+    });
+
+    // returning the CSV content via the "locations.csv" file
+    res
+      .set({
+        'Content-Type': 'text/csv',
+        'Content-Disposition': `attachment; filename="locations.csv"`,
+      })
+      .send(csvData);
   }
 }
